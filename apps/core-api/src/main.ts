bd1e3eeedf72b2b21afa31897e77f4493d8d5418
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2023-07-09 17:43:57 +0200

    feat: creating new app for actual api to get openapi spec from

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -7,6 +7,6 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 8000)
+  await app.listen(process.env.PORT ?? 8001)
 }
 void bootstrap()
