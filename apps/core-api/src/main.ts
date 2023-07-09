05da9e8edeb1598ca9b4e5eb716d97b39584015b
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2023-07-09 18:05:56 +0200

    feat: openapi integration

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -1,3 +1,5 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
 if (!process.env.IS_TS_NODE) {
   require('module-alias/register')
 }
@@ -7,6 +9,21 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Oktavolt REST API')
+    .setDescription(
+      'REST API and JSON interface for company Oktavolt. It contains the core functionality for the application.',
+    )
+    .addServer(
+      process.env.OPENAPI_SERVER_URL ?? '',
+      'Official Oktavolt REST API and JSON interface',
+    )
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 8001)
 }
 void bootstrap()
